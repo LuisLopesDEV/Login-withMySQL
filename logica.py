392d88f027dfa313e65d96bfa027be277563da7e
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2026-01-23 13:34:08 -0300

    Atualização gráfica do site
    Apenas estica, adicionei titulo nas paginas login e sign e mudei a cor da header

--- logica.py
+++ logica.py
@@ -3,21 +3,39 @@ import mysql.connector
 from hashlib import sha256
 
 
-def gerar_formulario():
-    formulario = Form(
+
+
+
+def gerar_formulario_sign():
+    formulario_sign = Form(
+        H1('Faça seu Cadastro', cls='titulo_form_sign'),
+        Input(type='text', cls='fname', name='fname', placeholder='Insira seu primeiro nome'),
+        Input(type='text', cls='lname', name='lname', placeholder='Insira seu ultimo nome'),
+        Input(type='date', cls='niver', name='niver', placeholder='Data de nascimento'),
+        Input( type='email', cls= 'email', name='email', placeholder='Informe seu email'),
+        Input(type='password',cls= 'password',name='password',placeholder='Informe sua senha'),
+        Button('Login',cls='envia',),
+        method='post',
+        action="/sign",
+        cls='form-sign'
+    )
+    return formulario_sign
+
+def gerar_formulario_login():
+    formulario_login = Form(
+        H1('Faça seu Login', cls='titulo_form_login'),
         Input( type='email', cls= 'email', name='email', placeholder='Informe seu email'),
         Input(type='password',cls= 'password',name='password',placeholder='Informe sua senha'),
         Button('Login',cls='envia',),
         method='post',
         action="/login",
-        cls='form-login'
-    )
-    return formulario
+        cls='form-login')
+    return formulario_login
 
 usuarios = {}  # dicionário {email: senha}
 
 
-def salvar_usuario(email, senha):
+def salvar_usuario(fname, lname, niver, email, senha):
     try:
         conexao = mysql.connector.connect(
             host="localhost",
@@ -26,11 +44,13 @@ def salvar_usuario(email, senha):
             database="email_market"
         )
         hashed_senha = sha256(senha.encode()).hexdigest()
+
         cursor = conexao.cursor()
-        comando = "INSERT INTO cadastro (Email, Senha) VALUES (%s, %s)"
-        cursor.execute(comando, (email, hashed_senha))
+
+        comando = "INSERT INTO cadastro (Primeiro_nome, Ultimo_nome, Nascimento, Email, Senha) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(comando, (fname, lname, niver, email, hashed_senha))
         conexao.commit()
-        print("Usuário salvo com sucesso!")
+
 
     except mysql.connector.Error as err:
         print("Erro MySQL:", err)  # Mostra a mensagem exata do MySQL
@@ -38,3 +58,31 @@ def salvar_usuario(email, senha):
     finally:
             cursor.close()
             conexao.close()
+
+def conferir_login(email, senha):
+    try:
+        conexao = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="email_market"
+        )
+        cursor = conexao.cursor()
+
+        comando = "SELECT Email, Senha FROM cadastro"
+        cursor.execute(comando)
+        usuarios = cursor.fetchall()
+        hashed_senha = sha256(senha.encode()).hexdigest()
+
+        for emails, senhas in usuarios:  # Desempacotamento automático
+            if emails == email and senhas == hashed_senha:
+                return True
+
+
+
+    except mysql.connector.Error as err:
+        print("Erro MySQL:", err)
+
+    finally:
+        cursor.close()
+        conexao.close()
--- main.py
+++ main.py
@@ -1,65 +1,91 @@
 from fasthtml.common import *
-from logica import gerar_formulario, salvar_usuario
-
+from logica import gerar_formulario_login ,gerar_formulario_sign, salvar_usuario, conferir_login
+from datetime import date
 app, routes = fast_app(static_dir='static', debug=True)
+from time import sleep
 
 @routes("/", methods=["get"])
 def layout_base(conteudo, titulo="Home"):
 
-    header = Nav(
-        Ul(Li(Strong("MinhaMarca", cls="logo"))),
-        Ul(
-            Li(A("Home", href="/")),
-            Li(A("Sobre", href="#")),
-            Li(A("Serviços", href="#")),
-            Li(A("Contato", href="#")),
-        ),
-        Ul(
-            Li(Button(A("Login", cls="btn-login", href="/login"))),
-            Li(Button(A("Sign Up", cls="btn-signup", href="/sign"))),
-        ),
-        cls="container-fluid"
-    )
-
     return (
         Title(titulo),
         Link(rel="stylesheet", href="/static/css/style.css"),
-        Div(
-            header,
-            Main(conteudo, cls="container"),  # Container centraliza o conteúdo
-            cls="page-wrapper"
-        )
-    )
+        Div(header,Main(conteudo, cls="container"), cls="page-wrapper"))
 
-@routes("/login", methods=["get"])
-def login_page():
-    formulario = gerar_formulario()
-    return Titled(
-        "Login",
+
+@routes("/sign", methods=["get"])
+def sign_page(titulo = 'Faça seu Cadastro'):
+    formulario_sign = gerar_formulario_sign()
+    return (Title(titulo),
         Link(rel="stylesheet", href="/static/css/style.css"),
-        Div(formulario, cls='center-page')
+        Div(header,formulario_sign, cls='center-page')
     )
 
-@routes("/login", methods=["post"])
-def login(email: str, password: str):
 
-    formulario = gerar_formulario()
+@routes("/sign", methods=["post"])
+def sign(fname: str, lname:str, niver: date,email: str, password: str):
+    formulario_sign = gerar_formulario_sign()
 
     if not email or not password:
-        return Titled(
-            "Login",
+        return (Title('Faça seu Cadastro'),
             Link(rel="stylesheet", href="/static/css/style.css"),
-            Div(formulario, P("Preencha os campos corretamente!", style="color: red", cls='txt_center-page'), cls='center-page')
+            Div(header, formulario_sign, P("Preencha os campos corretamente!", style="color: red", cls='txt_center-page'), cls='center-page')
         )
     try:
-        salvar_usuario(email, password)
+        salvar_usuario(fname, lname, niver, email, password)
 
-        return Titled(
-            "Login",
+        return (Title('Faça seu Cadastro'),
             Link(rel="stylesheet", href="/static/css/style.css"),
-            Div(formulario, P("Login realizado", style="color: blue", cls='txt_center-page'), cls='center-page')
+            Div(header,formulario_sign, P("Cadastro realizado", style="color: blue", cls='txt_center-page'), cls='center-page')
         )
     except Exception as e:
         return layout_base(P(f"Erro ao salvar: {e}", style="color: red"))
 
+
+@routes("/login", methods=["get"])
+def login_page():
+
+    formulario_login = gerar_formulario_login()
+
+    return (Title('Faça seu Login'),
+                  Div(header, formulario_login, cls="center-page"),
+                  Link(rel="stylesheet", href="/static/css/style.css"))
+
+
+@routes("/login", methods=["post"])
+def login(email:str, password:str):
+    formulario_login = gerar_formulario_login()
+
+    if not email or not password:
+        return (Title('Faça seu Login'),
+                      Div(header, formulario_login,
+                          P("Preencha os campos corretamente!", style="color: red", cls='txt_center-page'),
+                          cls="center-page"),
+                      Link(rel="stylesheet", href="/static/css/style.css"))
+    try:
+        resultado = conferir_login(email, password)
+        if resultado:
+            return (Title('Faça seu Login'),
+                          Meta(http_equiv="refresh", content="3; url=/"),
+                          Link(rel="stylesheet", href="/static/css/style.css"),
+                          Div(header, formulario_login,
+                              P("Login realizado com sucesso!", style="color: blue"),
+                              P("Aguarde, você será redirecionado em instantes..."),
+                              cls="center-page"))
+        else:
+            return (Title('Faça seu Login'),
+                          Div(header, formulario_login, P('Senha incorreta!', cls='txt_center-page'),
+                              cls="center-page"),
+                          Link(rel="stylesheet", href="/static/css/style.css"))
+    except Exception as e:
+        return Titled('Faça seu Login',
+                      Div(header, formulario_login,
+                          P(f'Erro ao salvar {e}', cls='txt_center-page', style="color: red"),
+                          cls="center-page"))
 serve()
+
+header = Nav(
+        Ul(Li(Strong("MinhaMarca", cls="logo"))),
+        Ul(Li(A("Home", href="/")),Li(A("Sobre", href="#")),Li(A("Serviços", href="#")),Li(A("Contato", href="#"))),
+        Ul(Li(Button(A("Login", cls="btn-login", href="/login"))),Li(Button(A("Sign Up", cls="btn-signup", href="/sign")))),
+        cls="container-fluid")
